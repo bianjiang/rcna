2ebccab5d002ec6747462458bed7caa7dfed3809
Jiang Bian <jbian@uams.edu>
2013-06-20 04:01:04 -0500

    update Readme.md

--- rcna/powerlaw_degree_distribution.py
+++ rcna/powerlaw_degree_distribution.py
@@ -22,16 +22,17 @@ import scipy.stats
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG, format='%(levelname)s: %(message)s')
-hdlr = logging.FileHandler('./powerlaw_degree_distribution.log')
+hdlr = logging.FileHandler('./powerlaw_degree_distribution.2.log')
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 hdlr.setFormatter(formatter)
 logger.addHandler(hdlr)
 
 def get_data(budgetYears):
-	network = load_network_for(range(2006,2010))
+	network = load_network_for(budgetYears)
 
 	wg = network.g.copy()
-	GrantResearcherNetwork.simplify(wg)
+
+	#wg = GrantResearcherNetwork.largest_component(wg)
 
 	degree = np.array(wg.degree(), dtype=np.int)
 
@@ -42,40 +43,33 @@ def get_data(budgetYears):
 
 def powerlaw_fit_p_value(budgetYears):	
 
+	logger.info("================================================================")
+	logger.info(budgetYears)
 	wg, degree, strength = get_data(budgetYears)
 
-	wg = GrantResearcherNetwork.largest_component(wg)
-	logger.info(wg.summary())
-
-
-	#save_to_R(degree, '%s/data/degree'%(root_folder()))
-	#save_to_R(strength, '%s/data/strength'%(root_folder()))
 
 	from network_analysis.powerlaw import plpva
 	import powerlaw
 
-	logger.info("degree p-value")
+	
 	fit = powerlaw.Fit(degree, discrete=True)
-	logger.info(fit.power_law.alpha)
-	logger.info(fit.power_law.xmin)
-	logger.info(fit.power_law.sigma)
-	logger.info(fit.power_law.D)
+	logger.info("test degree powerlaw distribution p-value; alpha: %f; xmin: %f; sigma: %f, D: %f"%(fit.power_law.alpha, fit.power_law.xmin, fit.power_law.sigma, fit.power_law.D))
 
-	logger.info(plpva.plpva(degree,fit.power_law.xmin))
+	p, gof = plpva.plpva(degree,fit.power_law.xmin)
 
-	logger.info("strength p-value")
+	logger.info("p: %f; good-of-fitness: %f"%(p, gof))
+
 	fit = powerlaw.Fit(strength, discrete=True)
-	logger.info(fit.power_law.alpha)
-	logger.info(fit.power_law.xmin)
-	logger.info(fit.power_law.sigma)
-	logger.info(fit.power_law.D)
+	logger.info("test strength powerlaw distribution p-value; alpha: %f; xmin: %f; sigma: %f, D: %f"%(fit.power_law.alpha, fit.power_law.xmin, fit.power_law.sigma, fit.power_law.D))
 
-	logger.info(plpva.plpva(strength, fit.power_law.xmin))
+	p, gof = plpva.plpva(strength, fit.power_law.xmin)
 
+	logger.info("p: %f; good-of-fitness: %f"%(p, gof))
 
 
 
 if __name__ == '__main__':
 
+	powerlaw_fit_p_value(range(2006,2010))
 	powerlaw_fit_p_value(range(2010,2013))
 
